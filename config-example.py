748bdd79289db12e9a2221688d4051b4a90e1bfc
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-10-26 20:20:28 -0500

    init

---
+++ config-example.py
@@ -0,0 +1,5 @@
+# DISCORD
+DISCORD_BOT_TOKEN: str = "TOKEN"
+
+# POSTGRES
+DATABASE_URI: str = "postgresql+psycopg2://USER:PASS@localhost:5432/db_name"
---
+++ run.py
@@ -0,0 +1,28 @@
+import asyncio
+import discord
+from discord.ext import commands, tasks
+from config import (
+    DISCORD_BOT_TOKEN,
+    PATH_TO_BIRTHDAY_QUOTES,
+)
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=".hp ", intents=intents, help_command=None)
+
+async def load_extensions():
+    extensions = []
+
+    for filename in extensions:
+        await bot.load_extension(filename)
+
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(DISCORD_BOT_TOKEN)
+
+
+# Main Bot Cycle
+asyncio.run(main())

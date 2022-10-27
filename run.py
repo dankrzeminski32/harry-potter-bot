9d4ec60df2b11cb3230307769cd4e284dc83dad4
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-10-26 21:01:20 -0500

    working foundation

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 import asyncio
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands 
 from config import (
     DISCORD_BOT_TOKEN,
-    PATH_TO_BIRTHDAY_QUOTES,
 )
+
 intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
 
 bot = commands.Bot(command_prefix=".hp ", intents=intents, help_command=None)
 
@@ -17,12 +15,10 @@ async def load_extensions():
     for filename in extensions:
         await bot.load_extension(filename)
 
-
 async def main():
     async with bot:
         await load_extensions()
         await bot.start(DISCORD_BOT_TOKEN)
-
 
 # Main Bot Cycle
 asyncio.run(main())

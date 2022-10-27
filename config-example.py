8ff671ab19f7b282450e04b3163ef227a7a42f2b
Ethan <ethankvachkoff@gmail.com>
2022-10-26 21:03:21 -0500

    fixed

--- config-example.py
+++ config-example.py
@@ -1,7 +1,5 @@
 # DISCORD
-DISCORD_BOT_TOKEN: str = (
-    "MTAxMjg0NTY3NjcwNDU4Mzc5MA.GJvuQt.4cSQg4d7IaZM92LjGOB-3XyTgW1dq3an_Ymm7U"
-)
+DISCORD_BOT_TOKEN: str = "TOKEN"
 
 # POSTGRES
 DATABASE_URI: str = "postgresql+psycopg2://USER:PASS@localhost:5432/db_name"
